de41be93f80acdee845ad05f94f25884a0005877
Namya Khan <82830866+namyakhan@users.noreply.github.com>
2022-07-21 22:19:51 +0530

    coding challenge #3

---
+++ toString.js
@@ -0,0 +1,11 @@
+
+//CHALLENGE❓
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+//SOLUTION🚀
+function booleanToString(b) {
+  return b.toString();
+}
