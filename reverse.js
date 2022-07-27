1fe09cd340bd01ff8e1a4d91d404f7c742eca426
Namya Khan <82830866+namyakhan@users.noreply.github.com>
2022-07-27 22:00:29 +0530

    coding challenge #11

---
+++ reverse.js
@@ -0,0 +1,10 @@
+//CHALLENGE❓
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+//SOLUTION🚀
+function solution(str) {
+  return str.split("").reverse().join("");
+}
