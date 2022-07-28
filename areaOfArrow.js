5f409ce7c5db1c4704c077483ba68083baf9404e
Namya Khan <82830866+namyakhan@users.noreply.github.com>
2022-07-28 19:53:59 +0530

    coding challenge #21

---
+++ areaOfArrow.js
@@ -0,0 +1,8 @@
+//CHALLENGE❓
+An arrow is formed in a rectangle with sides a and b by joining the bottom corners to the midpoint of the top edge and the centre of the rectangle.
+
+//SOLUTION🚀
+function arrowArea(a,b) {
+  // your code
+  return (a*b)/4
+}
