784bf4daecdbd0a904d35653d091238ef97b9c42
Namya Khan <82830866+namyakhan@users.noreply.github.com>
2022-08-13 23:16:50 +0530

    coding challenge #31

---
+++ lostWithoutMap.js
@@ -0,0 +1,14 @@
+//CHALLENGE❓
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+
+
+
+////SOLUTION🚀
+function maps(x){
+return x.map(num => num*2)
+}
