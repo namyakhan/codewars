9fcd78195382ca92c314941f0c756a8c46ef86d7
Namya Khan <82830866+namyakhan@users.noreply.github.com>
2022-08-16 23:10:19 +0530

    coding challenge #36

---
+++ oppositeNumber.js
@@ -0,0 +1,14 @@
+//CHALLENGE❓
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+
+////SOLUTION🚀
+function opposite(number) {
+ return (-number)
+}
