1be0186f817cef184601891f4c10d34967d8d1e9
Namya Khan <82830866+namyakhan@users.noreply.github.com>
2022-08-18 23:14:36 +0530

    coding challenge #38

---
+++ multiplying.js
@@ -0,0 +1,12 @@
+//CHALLENGE❓
+Implement a function called multiply, which takes two numbers and returns their product:
+
+multiply(2, 3) = 6
+multiply(0, 188) = 0
+multiply(85, 144) = 12240
+
+
+////SOLUTION🚀
+function multiply(a,b){
+  return a*b
+}
